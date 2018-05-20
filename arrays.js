f7aed06cd3dcc5881003b4e3af49b23eecf9c725
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-20 05:48:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function destructivelyAddElementToBeginningOfArray(inBoundArray, element) {
 }
 
 function addElementToEndOfArray(inBoundArray, element) {
-  var newArray = [element, ...inBoundArray]; //creates new array instead of altering existing
+  var newArray = [...inBoundArray, element]; //creates new array instead of altering existing
 
   return newArray;
 }
